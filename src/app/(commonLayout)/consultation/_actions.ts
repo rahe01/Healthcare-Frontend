685bfd0affa81a2f3ca5e1b402da832333ab70d6
rahe01 <rahatrahe01@gmail.com>
2026-02-28 23:04:04 +0600

    form btn

--- src/app/(commonLayout)/consultation/_actions.ts
+++ src/app/(commonLayout)/consultation/_actions.ts
@@ -1,9 +1,17 @@
+"use server";
+
 import { httpClient } from "@/lib/axios/httpClient";
 
-const getDoctors = async () => {
-    const doctors = await httpClient.get("/doctors");
-    return doctors;
+interface IDoctor {
+    id: number;
+    name: string;
+    specialization: string;
+    experience: number;
+    rating: number;
 }
 
-
-export { getDoctors };
+export const getDoctors = async () => {
+    const doctors = await httpClient.get<IDoctor[]>('/doctors');
+    console.log(doctors, "server");
+    return doctors;
+}
